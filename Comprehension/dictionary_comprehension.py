f120c623d00b9afae3b2c0966de17f8b91ee7d56
SaurabhSingh86 <saurabhsingh8602@gmail.com>
2022-02-04 02:34:27 +0530

    3nd stage

--- Comprehension/dictionary_comprehension.py
+++ Comprehension/dictionary_comprehension.py
@@ -1,4 +1,12 @@
+"""WAP to create a dictionary with item & its index pair"""
 
+# s = "python"
+#
+# d = {char: index for index, char in enumerate(s)}
+# print(d)
+
+
+"""WAP to create a dictionary with words & its length pair"""
 # s = "sky is no limit"
 
 # list_ = s.split()
@@ -11,10 +19,10 @@
 # # dict_ = {word: len(word) for word in list_}
 # # print(dict_)
 #
-#
+""" WAP to create a dictionary with character & its count pair """
 #
 # string = "hello world"
-# d = {}
+# # d = {}
 #
 # for char in string:
 #     if char not in d:
@@ -24,49 +32,49 @@
 # print(d)
 #
 # # comprehension
-from collections import defaultdict
-# dd = defaultdict(int)
-# d = {item : string.count(item) for item in string}
+
+# d = {item: string.count(item) for item in string}
 # print(d)
 
-# """wap """"
+# """WAP to create a dictionary of words & its count pair only if the words in of even length """"
 
 # s = "python is a language, python programming is easy"
-#
-# list_ = s.split()
-# dict_ = {word: list_.count(word) for word in list_}
-# print(dict_)
 
-# s = "python is a language, python programming is easy"
-# list_ = s.split()
-# d = {}
-# for word in list_:
-#     if len(word) % 2 == 0:
-#         d[word] = list_.count(word)
-# print(d)
-# print()
-# # by using comprehension
-#
 # d = {word: list_.count(word) for word in list_ if len(word) % 2 == 0}
 # print(d)
 
+""" WAP to create a dictionary with index & word pair if the word is of odd length reverse it else keep it as it is """
 
 # string = "python is a language, python programming is easy"
-# # l = string.split()
-# # dd = {index: word[::-1] if len(word) % 2 != 0 else word for index, word in enumerate(l)}
-# # print(dd)
+# l = string.split()
+# dd = {index: word[::-1] if len(word) % 2 != 0 else word for index, word in enumerate(l)}
+# print(dd)
+
+
+""" WAP to create a dictionary with word & its length pair only if the word starting with vowels """
 
 # string = "python is a language, python programming is easy"
 # l = string.split()
 # dd = {word: len(word) for word in l if word[0] in "aeiouAEIOU"}
 # print(dd)
 
-#
+""" WAP to create a dictionary of index & item pair if the item is of string data type reverse it & rest as it is """
+# l = ["python", 1, 2.5, "ruby", "vS code", 99, 3+0j]
+# d = {index: item if not isinstance(item, str) else str(item)[::-1] for index, item in enumerate(l)}
+# print(d)
+
+# d1 = {index: item[::-1] if isinstance(item, str) else item for index, item in enumerate(l)}
+# print(d1)
+
+""" WAP to create a dictionary of index & item pair if the item is of string data type keep it as it is else reverse it """
 # l = ["python", 1, 2.5, "ruby", "vS code", 99, 3+0j]
 # d = {index: item if isinstance(item, str) else str(item)[::-1] for index, item in enumerate(l)}
 # print(d)
+#
+# d1 = {index: str(item)[::-1] if not isinstance(item, str) else item for index, item in enumerate(l)}
+# print(d1)
 
-
+""" WA dictionary comprehension to flip or swap key & values in a dictionary """
 # d = {"a": 1, "b": 2, "c": 3, "d":4, "e": 5}
 #
 # dd = {value: key for key, value in d.items()}
@@ -76,11 +84,15 @@ from collections import defaultdict
 #
 # d2 = {d[key]: key for key in d}
 # print(d2)
-# """WAP to create a dict of character & its ascii value pair"""
+
+
+"""WAP to create a dict of character & its ascii value pair"""
 # s = "rainbow"
 # d = {char: ord(char) for char in s}
 # print(d)
-#
-# """WAP to create a dict of character as value & its ascii as key"""
+
+
+"""WAP to create a dict of character as value & its ascii as key"""
+
 # d1 = {ord(char): char for char in s}
 # print(d1)
