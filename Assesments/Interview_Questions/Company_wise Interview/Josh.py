a982b284c696cb059cfaadfe839494419bc4647c
Laptop <saurabhsingh86@gmail.com>
2022-07-27 19:27:38 +0530

    1st added

---
+++ Assesments/Interview_Questions/Company_wise Interview/Josh.py
@@ -0,0 +1,66 @@
+"""
+1)tell me about yourself and project explanation
+2) about webdriver
+3)how to write test cases in API testing
+4) current version of selenium
+5)how to get text in API testing
+
+what is tuple and dictionary?
+
+where do we use dictionaries in selenium?
+
+how do you store the data in python?
+
+what is the difference between driver. get and driver. navigate?
+
+How did you use API TESTING?
+
+Along with these go through the Basics of Testing  concepts Q&A.
+
+API basics Testing Q&A
+
+Python Selenium Q&A
+
+
+
+* How to create API
+
+* Automate http://reqres.in site using selenium
+
+* Difference b/w Tuple and Dictionary
+
+* A working example of Square root in Python
+
+Pythagoras theorem states that in a right-angled triangle, the side opposite to the right angle called the hypotenuse is measured as the square root of the sum of squares of measures of the other two sides, which means
+c=√(a2+b2) # where c is the hypotenuse
+
+Here is the solution in Python:
+
+from math import sqrt #Imported the square root function from math module
+
+ from math import pow #Imported the power function from math module
+
+ a=int(input("Enter the measure of one side of a right angled triangle:"))
+
+ b=int(input("Enter the measure of another side of a right angled triangle:"))
+
+ #input function is used to take input from user and is stored as string
+
+# which is then typecasted into an integer using the int() function.
+
+ c=sqrt(pow(a,2)+pow(b,2))
+
+ #we have implemented the formula c=√(a2+b2)
+
+ print(f"The measure of the hypotenuse is: {c} based on the measures of the other two sides {a} & {b}")
+
+
+OUTPUT:
+Enter the measure of one side of a right-angled triangle :3
+Enter the measure of another side of a right-angled triangle: 4
+The measure of the hypotenuse is: 5.0 based on the measures of the other two sides 3 & 4
+
+* Cube root in Python example.
+
+
+"""
