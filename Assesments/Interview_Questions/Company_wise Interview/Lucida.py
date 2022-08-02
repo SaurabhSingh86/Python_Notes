53c87dc4aa711ec30239cc05f980c92d110c6c75
Laptop <saurabhsingh86@gmail.com>
2022-08-02 23:13:25 +0530

    1st added

---
+++ Assesments/Interview_Questions/Company_wise Interview/Lucida.py
@@ -0,0 +1,18 @@
+numbers = {}
+letter = {}
+numbers[0] = 'hai'
+numbers[3] = 'hello'
+letter[2] = 'Python'
+comb = {}
+comb['Numbers'] = numbers
+comb["Letter"] = letter
+print(comb)
+
+# {'Numbers': {0: 'hai', 3: 'hello'}, 'Letter': {2: 'Python'}}
+
+
+# What is the output of os.getlogin()
+
+
+#
+
---
+++ Assesments/Interview_Questions/Company_wise Interview/infosys.py
@@ -0,0 +1,26 @@
+""" Infosys Interview questions """
+
+"""
+size of list    |     elements of list        |   Calculation         |   Output 
+    2                   [1, 2]                      2 - 1                   1
+    3                 [4, 2, 1] 
+    4
+
+"""
+
+"""
+[6, 5]              =>  6-5         => 1
+[5, 4]              =>  5-4         => 1
+[4, 7]              =>  7-4         => 3
+[6, 5, 4]           =>  6-5         => 1
+[6, 5, 4, 7]        => 7-6          => 1
+print Total 3 distinct values
+
+"""
+
+
+"""
+[4, 2, 1]           => 2
+[1, 2, 3]           => 1
+[6, 5, 4, 7]        => 3
+"""
